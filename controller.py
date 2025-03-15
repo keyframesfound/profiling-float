41c5566becc64d6d2c84dd7bea3ce15ab6d61e4a
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2025-03-15 13:57:09 +0800

    Update controller.py: Disable debug mode for production

--- controller.py
+++ controller.py
@@ -36,4 +36,4 @@ def home():
 
 if __name__ == '__main__':
     # Run on all interfaces at port 3900.
-    app.run(debug=True, host='0.0.0.0', port=3900)
+    app.run(debug=False, host='0.0.0.0', port=3900)
