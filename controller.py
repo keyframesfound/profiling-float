cf5be1a8c16796b9e170ecb4254a5473f74ec25b
keyframesfound <80465725+keyframesfound@users.noreply.github.com>
2025-03-17 16:26:37 +0800

    Fix button URLs in controller.py to include HTTP protocol

--- controller.py
+++ controller.py
@@ -19,10 +19,10 @@ html_template = '''
 <body>
     <h1>Float Control Pannel</h1>
     <!-- Button to open a specific IP webpage -->
-    <button onclick="window.open('192.168.4.1/control', '_blank');">Sink Float</button>
+    <button onclick="window.open('http://192.168.4.1/control', '_blank');">Sink Float</button>
     
     <!-- Button to open a specific webpage -->
-    <button onclick="window.open('192.168.4.1/data', '_blank');">Float Pressure Data</button>
+    <button onclick="window.open('http://192.168.4.1/data', '_blank');">Float Pressure Data</button>
     
     <!-- Third button that opens another specific webpage -->
     <button onclick="window.open('http://192.168.1.101', '_blank');">Generate Graph</button>
